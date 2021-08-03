e53961880dbb6d47af8627a736003c45cba9cd86
curiousyuvi <ysgaur9919@gmail.com>
2021-08-04 04:30:46 +0530

    added GridView to RankingScreen

--- lib/rankingScreen.dart
+++ lib/rankingScreen.dart
@@ -22,120 +22,122 @@ class _RankingScreenState extends State<RankingScreen> {
         child: Padding(
           padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.04),
           child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Choose Platform',
-                  style: TextStyle(
-                      color: ColorSchemeClass.lightgrey,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'young',
-                      fontSize: MediaQuery.of(context).size.height * 0.033),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.width * 0.35,
-                      width: MediaQuery.of(context).size.width * 0.35,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: ColorSchemeClass.codeforcespurple
-                                  .withOpacity(0.5),
-                              width: 2),
-                          boxShadow: [
-                            BoxShadow(
+              height: double.infinity,
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Choose Platform',
+                    style: TextStyle(
+                        color: ColorSchemeClass.lightgrey,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'young',
+                        fontSize: MediaQuery.of(context).size.height * 0.033),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  GridView.count(
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    mainAxisSpacing: MediaQuery.of(context).size.width * 0.1,
+                    crossAxisSpacing: MediaQuery.of(context).size.width * 0.1,
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.width * 0.35,
+                        width: MediaQuery.of(context).size.width * 0.35,
+                        decoration: BoxDecoration(
+                            border: Border.all(
                                 color: ColorSchemeClass.codeforcespurple
-                                    .withOpacity(0.2),
-                                offset: Offset(0,
-                                    MediaQuery.of(context).size.height * 0.007))
-                          ],
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image:
-                                  AssetImage('images/codeforcesSquare.jpg'))),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.1,
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.width * 0.35,
-                      width: MediaQuery.of(context).size.width * 0.35,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: ColorSchemeClass.codechefbrown
-                                  .withOpacity(0.5),
-                              width: 2),
-                          boxShadow: [
-                            BoxShadow(
+                                    .withOpacity(0.5),
+                                width: 2),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: ColorSchemeClass.codeforcespurple
+                                      .withOpacity(0.2),
+                                  offset: Offset(
+                                      0,
+                                      MediaQuery.of(context).size.height *
+                                          0.007))
+                            ],
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image:
+                                    AssetImage('images/codeforcesSquare.jpg'))),
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.width * 0.35,
+                        width: MediaQuery.of(context).size.width * 0.35,
+                        decoration: BoxDecoration(
+                            border: Border.all(
                                 color: ColorSchemeClass.codechefbrown
-                                    .withOpacity(0.2),
-                                offset: Offset(0,
-                                    MediaQuery.of(context).size.height * 0.007))
-                          ],
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: AssetImage('images/codechefSquare.jpg'))),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.05,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.width * 0.35,
-                      width: MediaQuery.of(context).size.width * 0.35,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color:
-                                  ColorSchemeClass.atcodergrey.withOpacity(0.5),
-                              width: 2),
-                          boxShadow: [
-                            BoxShadow(
+                                    .withOpacity(0.5),
+                                width: 2),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: ColorSchemeClass.codechefbrown
+                                      .withOpacity(0.2),
+                                  offset: Offset(
+                                      0,
+                                      MediaQuery.of(context).size.height *
+                                          0.007))
+                            ],
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image:
+                                    AssetImage('images/codechefSquare.jpg'))),
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.width * 0.35,
+                        width: MediaQuery.of(context).size.width * 0.35,
+                        decoration: BoxDecoration(
+                            border: Border.all(
                                 color: ColorSchemeClass.atcodergrey
-                                    .withOpacity(0.2),
-                                offset: Offset(0,
-                                    MediaQuery.of(context).size.height * 0.007))
-                          ],
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: AssetImage('images/atcoderSquare.jpg'))),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.1,
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.width * 0.35,
-                      width: MediaQuery.of(context).size.width * 0.35,
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                              color: ColorSchemeClass.spojblue.withOpacity(0.5),
-                              width: 2),
-                          boxShadow: [
-                            BoxShadow(
+                                    .withOpacity(0.5),
+                                width: 2),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: ColorSchemeClass.atcodergrey
+                                      .withOpacity(0.2),
+                                  offset: Offset(
+                                      0,
+                                      MediaQuery.of(context).size.height *
+                                          0.007))
+                            ],
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: AssetImage('images/atcoderSquare.jpg'))),
+                      ),
+                      Container(
+                        height: MediaQuery.of(context).size.width * 0.35,
+                        width: MediaQuery.of(context).size.width * 0.35,
+                        decoration: BoxDecoration(
+                            border: Border.all(
                                 color:
-                                    ColorSchemeClass.spojblue.withOpacity(0.2),
-                                offset: Offset(0,
-                                    MediaQuery.of(context).size.height * 0.007))
-                          ],
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                              image: AssetImage('images/spojSquare.jpg'))),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
+                                    ColorSchemeClass.spojblue.withOpacity(0.5),
+                                width: 2),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: ColorSchemeClass.spojblue
+                                      .withOpacity(0.2),
+                                  offset: Offset(
+                                      0,
+                                      MediaQuery.of(context).size.height *
+                                          0.007))
+                            ],
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: AssetImage('images/spojSquare.jpg'))),
+                      )
+                    ],
+                  ),
+                ],
+              )),
         ),
       ),
     );
   }
 }
+
+/* */
