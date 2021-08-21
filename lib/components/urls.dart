9ef6e7fac2280790361b2960b7a926f458a0c84c
curiousyuvi <ysgaur9919@gmail.com>
2021-08-21 19:57:47 +0530

    fixed avatar urls

--- lib/components/urls.dart
+++ lib/components/urls.dart
@@ -1,16 +1,16 @@
 class Urls {
   static String avatar1url =
-      "https://firebasestorage.googleapis.com/v0/b/coder-status.appspot.com/o/avatar1.jpg?alt=media&token=aacb9806-2371-4c0a-96ab-a7016f28f1c6";
+      "https://firebasestorage.googleapis.com/v0/b/coderstatus.appspot.com/o/avatar1.jpg?alt=media&token=e09f9c48-1f5b-44d7-8401-6410e3623e53";
   static String avatar2url =
-      "https://firebasestorage.googleapis.com/v0/b/coder-status.appspot.com/o/avatar2.jpg?alt=media&token=66ed2a5e-6bd8-4530-a5b7-61476402588a";
+      "https://firebasestorage.googleapis.com/v0/b/coderstatus.appspot.com/o/avatar2.jpg?alt=media&token=d8129ee5-3e43-44fa-8ef6-15cb1e76f9b7";
   static String avatar3url =
-      "https://firebasestorage.googleapis.com/v0/b/coder-status.appspot.com/o/avatar3.jpg?alt=media&token=8922d8d7-ebb7-4bff-928a-10f8a99a9862";
+      "https://firebasestorage.googleapis.com/v0/b/coderstatus.appspot.com/o/avatar3.jpg?alt=media&token=4fbe71fa-8051-472e-873f-417ffe78a055";
   static String avatar4url =
-      "https://firebasestorage.googleapis.com/v0/b/coder-status.appspot.com/o/avatar4.jpg?alt=media&token=b8c6dbf9-80a3-4383-af0c-3b610f484367";
+      "https://firebasestorage.googleapis.com/v0/b/coderstatus.appspot.com/o/avatar4.jpg?alt=media&token=8fd037b8-2ec1-4445-b736-c6173db86c85";
   static String avatar5url =
-      "https://firebasestorage.googleapis.com/v0/b/coder-status.appspot.com/o/avatar5.jpg?alt=media&token=139a4e30-3952-4177-a1e7-573052398bea";
+      "https://firebasestorage.googleapis.com/v0/b/coderstatus.appspot.com/o/avatar5.jpg?alt=media&token=ce62004a-4084-4789-9213-86074dd6c9a5";
   static String avatar6url =
-      "https://firebasestorage.googleapis.com/v0/b/coder-status.appspot.com/o/avatar6.jpg?alt=media&token=c5f68226-90a8-4380-8cd0-19db9ecf830f";
+      "https://firebasestorage.googleapis.com/v0/b/coderstatus.appspot.com/o/avatar6.jpg?alt=media&token=c2ae7f5a-b04c-466c-942f-41540885fa3d";
   static String avatar7url =
-      "https://firebasestorage.googleapis.com/v0/b/coder-status.appspot.com/o/avatar7.jpg?alt=media&token=3e0681db-81a3-4a33-b83c-ca6320f85962";
+      "https://firebasestorage.googleapis.com/v0/b/coderstatus.appspot.com/o/avatar7.jpg?alt=media&token=4e032cb6-12fe-4501-bfca-ac118393a949";
 }
